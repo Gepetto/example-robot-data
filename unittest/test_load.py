9c6a8ee4b48a1989c8d628868f8e77b680c64240
Guilhem Saurel <guilhem.saurel@gmail.com>
2019-02-26 14:26:07 +0100

    [Test] Standardization

--- unittest/test_load.py
+++ unittest/test_load.py
@@ -1,5 +1,8 @@
-from unittest_utils import loadTalosArm, loadTalos, loadHyQ
+#!/usr/bin/env python2
+
 import unittest
+
+from unittest_utils import loadHyQ, loadTalos, loadTalosArm
 
 
 class RobotTestCase(unittest.TestCase):
@@ -16,28 +19,31 @@ class RobotTestCase(unittest.TestCase):
         self.assertEqual(model.nv, self.NV, "Wrong nv value.")
 
     def test_q0(self):
-      self.assertTrue(hasattr(self.ROBOT, "q0"), "It doesn't have q0")
+        self.assertTrue(hasattr(self.ROBOT, "q0"), "It doesn't have q0")
+
 
 class TalosArmTest(RobotTestCase):
     RobotTestCase.ROBOT = loadTalosArm()
     RobotTestCase.NQ = 7
     RobotTestCase.NV = 7
 
+
 class TalosArmFloatingTest(RobotTestCase):
     RobotTestCase.ROBOT = loadTalosArm()
     RobotTestCase.NQ = 14
     RobotTestCase.NV = 13
+
 
 class TalosTest(RobotTestCase):
     RobotTestCase.ROBOT = loadTalos()
     RobotTestCase.NQ = 39
     RobotTestCase.NV = 38
 
+
 class HyQTest(RobotTestCase):
     RobotTestCase.ROBOT = loadHyQ()
     RobotTestCase.NQ = 19
     RobotTestCase.NV = 18
-
 
 
 if __name__ == '__main__':
--- unittest/unittest_utils.py
+++ unittest/unittest_utils.py
@@ -1,38 +1,39 @@
+import numpy as np
+
 import pinocchio
-from pinocchio.utils import *
 from pinocchio.robot_wrapper import RobotWrapper
 
 
-
 def readParamsFromSrdf(robot, SRDF_PATH, verbose):
-  rmodel = robot.model
+    rmodel = robot.model
 
-  pinocchio.loadRotorParameters(rmodel, SRDF_PATH, verbose)
-  rmodel.armature = \
-                    np.multiply(rmodel.rotorInertia.flat, np.square(rmodel.rotorGearRatio.flat))
-  try:
-    pinocchio.loadReferenceConfigurations(rmodel, SRDF_PATH, verbose)
-    robot.q0.flat[:] = rmodel.referenceConfigurations["half_sitting"].copy()
-  except:
-    print "loadReferenceConfigurations did not work. Please check your Pinocchio Version"
+    pinocchio.loadRotorParameters(rmodel, SRDF_PATH, verbose)
+    rmodel.armature = np.multiply(rmodel.rotorInertia.flat, np.square(rmodel.rotorGearRatio.flat))
     try:
-      pinocchio.getNeutralConfiguration(rmodel, SRDF_PATH, verbose)
-      robot.q0.flat[:] = rmodel.neutralConfiguration.copy()
+        pinocchio.loadReferenceConfigurations(rmodel, SRDF_PATH, verbose)
+        robot.q0.flat[:] = rmodel.referenceConfigurations["half_sitting"].copy()
     except:
-      robot.q0.flat[:] = pinocchio.neutral(rmodel)
-  return
-  
+        print("loadReferenceConfigurations did not work. Please check your Pinocchio Version")
+        try:
+            pinocchio.getNeutralConfiguration(rmodel, SRDF_PATH, verbose)
+            robot.q0.flat[:] = rmodel.neutralConfiguration.copy()
+        except:
+            robot.q0.flat[:] = pinocchio.neutral(rmodel)
+    return
+
+
 def loadTalosArm(modelPath='/opt/openrobots/share/example-robot-data'):
     URDF_FILENAME = "talos_left_arm.urdf"
     URDF_SUBPATH = "/talos/" + URDF_FILENAME
     SRDF_FILENAME = "talos.srdf"
     SRDF_SUBPATH = "/talos/" + SRDF_FILENAME
     # Load URDF file
-    robot = RobotWrapper.BuildFromURDF(modelPath+URDF_SUBPATH, [modelPath])
+    robot = RobotWrapper(modelPath + URDF_SUBPATH, [modelPath])
 
     # Load SRDF file
-    readParamsFromSrdf(robot, modelPath+SRDF_SUBPATH, False)
+    readParamsFromSrdf(robot, modelPath + SRDF_SUBPATH, False)
     return robot
+
 
 def loadTalos(modelPath='/opt/openrobots/share/example-robot-data'):
     URDF_FILENAME = "talos_reduced.urdf"
@@ -40,11 +41,10 @@ def loadTalos(modelPath='/opt/openrobots/share/example-robot-data'):
     SRDF_SUBPATH = "/talos/" + SRDF_FILENAME
     URDF_SUBPATH = "/talos/" + URDF_FILENAME
     # Load URDF file
-    robot = RobotWrapper.BuildFromURDF(modelPath+URDF_SUBPATH, [modelPath],
-                                                               pinocchio.JointModelFreeFlyer())
+    robot = RobotWrapper.BuildFromURDF(modelPath + URDF_SUBPATH, [modelPath], pinocchio.JointModelFreeFlyer())
     # Load SRDF file
-    readParamsFromSrdf(robot, modelPath+SRDF_SUBPATH, False)
-    assert((robot.model.armature[:6]==0.).all())
+    readParamsFromSrdf(robot, modelPath + SRDF_SUBPATH, False)
+    assert ((robot.model.armature[:6] == 0.).all())
     return robot
 
 
@@ -53,8 +53,7 @@ def loadHyQ(modelPath='/opt/openrobots/share/example-robot-data'):
     import os
     URDF_FILENAME = "hyq_no_sensors.urdf"
     URDF_SUBPATH = "/hyq/" + URDF_FILENAME
-    robot = RobotWrapper.BuildFromURDF(modelPath+URDF_SUBPATH, [modelPath],
-                                       pinocchio.JointModelFreeFlyer())
+    robot = RobotWrapper.BuildFromURDF(modelPath + URDF_SUBPATH, [modelPath], pinocchio.JointModelFreeFlyer())
     # TODO define default position inside srdf
     robot.q0.flat[7:] = [-0.2, 0.75, -1.5, -0.2, -0.75, 1.5, -0.2, 0.75, -1.5, -0.2, -0.75, 1.5]
     robot.q0[2] = 0.57750958
