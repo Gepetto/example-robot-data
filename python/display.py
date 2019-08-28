8d37abd977cf1cea1087621c0f8c47c2accbc1ac
Carlos Mastalli <carlos.mastalli@laas.fr>
2019-08-28 13:59:00 +0200

    [cleanup] Removed usless system path append + import the entire example_robot_data module

--- python/display.py
+++ python/display.py
@@ -1,8 +1,6 @@
 import sys
 
-from example_robot_data import loadHyQ, loadICub, loadTalos, loadTalosArm, loadTiago, loadTiagoNoHand
-
-sys.path.append('/opt/openrobots/share/example-robot-data/unittest/')
+import example_robot_data
 
 DISPLAY_HYQ = 'hyq' in sys.argv
 DISPLAY_TALOS = 'talos' in sys.argv
@@ -12,31 +10,31 @@ DISPLAY_TIAGO_NO_HAND = 'tiago_no_hand' in sys.argv
 DISPLAY_ICUB = 'icub' in sys.argv
 
 if DISPLAY_HYQ:
-    hyq = loadHyQ()
+    hyq = example_robot_data.loadHyQ()
     hyq.initDisplay(loadModel=True)
     hyq.display(hyq.q0)
 
 if DISPLAY_TALOS:
-    talos = loadTalos()
+    talos = example_robot_data.loadTalos()
     talos.initDisplay(loadModel=True)
     talos.display(talos.q0)
 
 if DISPLAY_TALOS_ARM:
-    talos_arm = loadTalosArm()
+    talos_arm = example_robot_data.loadTalosArm()
     talos_arm.initDisplay(loadModel=True)
     talos_arm.display(talos_arm.q0)
 
 if DISPLAY_TIAGO:
-    tiago = loadTiago()
+    tiago = example_robot_data.loadTiago()
     tiago.initDisplay(loadModel=True)
     tiago.display(tiago.q0)
 
 if DISPLAY_TIAGO_NO_HAND:
-    tiago_no_hand = loadTiagoNoHand()
+    tiago_no_hand = example_robot_data.loadTiagoNoHand()
     tiago_no_hand.initDisplay(loadModel=True)
     tiago_no_hand.display(tiago_no_hand.q0)
 
 if DISPLAY_ICUB:
-    icub = loadICub()
+    icub = example_robot_data.loadICub()
     icub.initDisplay(loadModel=True)
     icub.display(icub.q0)
