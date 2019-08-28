842a943a430a2321e5e7432b19feb63f27798817
Carlos Mastalli <carlos.mastalli@laas.fr>
2019-08-28 14:11:31 +0200

    [display] Updated a deprecated pinocchio function

--- python/display.py
+++ python/display.py
@@ -11,30 +11,30 @@ DISPLAY_ICUB = 'icub' in sys.argv
 
 if DISPLAY_HYQ:
     hyq = example_robot_data.loadHyQ()
-    hyq.initDisplay(loadModel=True)
+    hyq.initViewer(loadModel=True)
     hyq.display(hyq.q0)
 
 if DISPLAY_TALOS:
     talos = example_robot_data.loadTalos()
-    talos.initDisplay(loadModel=True)
+    talos.initViewer(loadModel=True)
     talos.display(talos.q0)
 
 if DISPLAY_TALOS_ARM:
     talos_arm = example_robot_data.loadTalosArm()
-    talos_arm.initDisplay(loadModel=True)
+    talos_arm.initViewer(loadModel=True)
     talos_arm.display(talos_arm.q0)
 
 if DISPLAY_TIAGO:
     tiago = example_robot_data.loadTiago()
-    tiago.initDisplay(loadModel=True)
+    tiago.initViewer(loadModel=True)
     tiago.display(tiago.q0)
 
 if DISPLAY_TIAGO_NO_HAND:
     tiago_no_hand = example_robot_data.loadTiagoNoHand()
-    tiago_no_hand.initDisplay(loadModel=True)
+    tiago_no_hand.initViewer(loadModel=True)
     tiago_no_hand.display(tiago_no_hand.q0)
 
 if DISPLAY_ICUB:
     icub = example_robot_data.loadICub()
-    icub.initDisplay(loadModel=True)
+    icub.initViewer(loadModel=True)
     icub.display(icub.q0)
