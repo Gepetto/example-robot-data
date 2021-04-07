afa3e74bf9b92a7a5e67268c045c560af65b5db2
Carlos Mastalli <carlos.mastalli@ed.ac.uk>
2021-04-07 10:30:54 +0100

    [cleanup] Formatted the code

--- python/example_robot_data/robots_loader.py
+++ python/example_robot_data/robots_loader.py
@@ -66,8 +66,8 @@ class RobotLoader(object):
 
         if self.srdf_filename:
             self.srdf_path = join(self.model_path, self.path, self.srdf_subpath, self.srdf_filename)
-            self.robot.q0 = readParamsFromSrdf(self.robot.model, self.srdf_path, self.verbose, self.has_rotor_parameters,
-                                         self.ref_posture)
+            self.robot.q0 = readParamsFromSrdf(self.robot.model, self.srdf_path, self.verbose,
+                                               self.has_rotor_parameters, self.ref_posture)
         else:
             self.srdf_path = None
             self.robot.q0 = None
@@ -182,8 +182,8 @@ class TalosLegsLoader(TalosLoader):
         self.robot.visual_data = pin.GeometryData(g2)
 
         # Load SRDF file
-        self.robot.q0 = readParamsFromSrdf(self.robot.model, self.srdf_path, self.verbose, self.has_rotor_parameters,
-                                     self.ref_posture)
+        self.robot.q0 = readParamsFromSrdf(self.robot.model, self.srdf_path, self.verbose,
+                                           self.has_rotor_parameters, self.ref_posture)
 
         assert (m2.armature[:6] == 0.).all()
         # Add the free-flyer joint limits to the new model
