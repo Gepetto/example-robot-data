3c03e668c3ad24a337bb10fca68bf39da3ac7fda
Guilhem Saurel <guilhem.saurel@laas.fr>
2023-05-14 12:30:43 +0200

    use CMake for EXAMPLE_ROBOT_DATA_MODEL_DIR

---
+++ include/example-robot-data/path.hpp
@@ -0,0 +1,9 @@
+#ifndef EXAMPLE_ROBOT_DATA_PATH_HPP
+#define EXAMPLE_ROBOT_DATA_PATH_HPP
+
+#include <example-robot-data/warning.hh>
+
+#pragma message EXAMPLE_ROBOT_DATA_WARN( \
+        "Header `example-robot-data/path.hpp` is deprecated and useless!")
+
+#endif
